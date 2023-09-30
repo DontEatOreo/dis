353c5e0e8f4b59ee27d1ed8806c3455cfb9be5a0
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2023-09-30 22:40:00 +0300

    feat: added Twitter downloader

--- CommandLineApp/Downloaders/VideoDownloaderFactory.cs
+++ CommandLineApp/Downloaders/VideoDownloaderFactory.cs
@@ -11,6 +11,9 @@ public class VideoDownloaderFactory : IDownloaderFactory
     private const string TikTokUrlPart = "tiktok";
     private const string YouTubeUrlPart = "youtu";
     private const string RedditUrlPart = "redd";
+    
+    private const string TwitterUrlPart = "twitter";
+    private const string XUrlPart = "x.com"; // New Twitter URL
 
     private const string FormatSort = "vcodec:h264,ext:mp4:m4a";
 
@@ -28,6 +31,8 @@ public class VideoDownloaderFactory : IDownloaderFactory
             { TikTokUrlPart, downloadQuery => new TikTokDownloader(_youtubeDl, downloadQuery, o.Options.KeepWatermark) },
             { YouTubeUrlPart, downloadQuery => new YouTubeDownloader(_youtubeDl, downloadQuery) },
             { RedditUrlPart, downloadQuery => new RedditDownloader(_youtubeDl, downloadQuery) },
+            { TwitterUrlPart, downloadQuery => new TwitterDownloader(_youtubeDl, downloadQuery)},
+            { XUrlPart, downloadQuery => new TwitterDownloader(_youtubeDl, downloadQuery)}
         };
 
         var optionSet = GenerateOptionSet(o);
