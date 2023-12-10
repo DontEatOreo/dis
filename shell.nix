31df4e8ef41b400f49254311760ffdad8f264fee
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2023-12-10 16:51:31 +0200

    fix: apply temporary fix for H.264 codec on darwin

--- shell.nix
+++ shell.nix
@@ -1,9 +1,25 @@
-with import <nixpkgs> {};
-  mkShell {
+let
+  pkgs = import <nixpkgs> {};
+  ffmpeg-patched =
+    # This is a temporary fix for broken H.264 codec on darwin (macOS)
+    if pkgs.stdenv.isDarwin
+    then
+      pkgs.ffmpeg.override {
+        x264 = pkgs.x264.overrideAttrs (old: {
+          postPatch =
+            old.postPatch
+            + ''
+              substituteInPlace Makefile --replace '$(if $(STRIP), $(STRIP) -x $@)' '$(if $(STRIP), $(STRIP) -S $@)'
+            '';
+        });
+      }
+    else pkgs.ffmpeg;
+in
+  pkgs.mkShell {
     name = "dotnet-env";
-    packages = [
+    packages = with pkgs; [
       dotnet-sdk_8
       yt-dlp
-      ffmpeg
+      ffmpeg-patched
     ];
   }
