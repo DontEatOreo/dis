b53f6a291a3ad17e749b65834299469314872a5b
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2024-09-03 16:05:57 +0300

    flake: format with `nixfmt-rfc-style`

--- flake.nix
+++ flake.nix
@@ -11,64 +11,86 @@
     extra-substituters = "https://devenv.cachix.org";
   };
 
-  outputs = inputs @ {
-    self,
-    devenv,
-    nixpkgs,
-  }: let
-    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
-    forEachSupportedSystem = f:
-      nixpkgs.lib.genAttrs supportedSystems (system:
-        f {
-          pkgs = import nixpkgs {inherit system;};
-        });
-  in {
-    devShells = forEachSupportedSystem ({pkgs, ...}: {
-      default = devenv.lib.mkShell {
-        inherit inputs pkgs;
-        modules = [
-          ({pkgs, ...}: {
-            languages.dotnet = {
-              enable = true;
-              package = pkgs.dotnetCorePackages.combinePackages (builtins.attrValues {
-                inherit (pkgs.dotnetCorePackages) sdk_8_0;
-              });
+  outputs =
+    inputs@{
+      self,
+      devenv,
+      nixpkgs,
+    }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import nixpkgs { inherit system; };
+          }
+        );
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs, ... }:
+        {
+          default = devenv.lib.mkShell {
+            inherit inputs pkgs;
+            modules = [
+              (
+                { pkgs, ... }:
+                {
+                  languages.dotnet = {
+                    enable = true;
+                    package = pkgs.dotnetCorePackages.combinePackages (
+                      builtins.attrValues {
+                        inherit (pkgs.dotnetCorePackages) sdk_8_0;
+                      }
+                    );
+                  };
+                }
+              )
+            ];
+          };
+        }
+      );
+      packages = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.buildDotnetModule {
+            pname = "dis";
+            version = "9.1.1";
+
+            src = ./.;
+
+            projectFile = "./dis.csproj";
+            nugetDeps = ./deps.nix;
+
+            dotnet-sdk = pkgs.dotnetCorePackages.sdk_8_0;
+            selfContainedBuild = true;
+
+            executables = [ "dis" ];
+
+            postFixup = ''
+              makeWrapper ${pkgs.dotnetCorePackages.sdk_8_0}/bin/dotnet $out/bin/dis --add-flags $out/lib/dis/dis.dll
+              wrapProgram "$out/bin/dis" \
+                --prefix PATH : ${pkgs.ffmpeg_6-full}/bin \
+                --prefix PATH : ${pkgs.yt-dlp}/bin
+            '';
+
+            runtimeDeps = [ pkgs.icu ];
+
+            meta = {
+              homepage = "https://github.com/DontEatOreo/dis";
+              license = pkgs.lib.licenses.agpl3Plus;
+              platforms = pkgs.lib.platforms.unix;
+              maintainers = [ pkgs.lib.maintainers.donteatoreo ];
             };
-          })
-        ];
-      };
-    });
-    packages = forEachSupportedSystem ({pkgs}: {
-      default = pkgs.buildDotnetModule {
-        pname = "dis";
-        version = "9.1.1";
-
-        src = ./.;
-
-        projectFile = "./dis.csproj";
-        nugetDeps = ./deps.nix;
-
-        dotnet-sdk = pkgs.dotnetCorePackages.sdk_8_0;
-        selfContainedBuild = true;
-
-        executables = ["dis"];
-
-        postFixup = ''
-          makeWrapper ${pkgs.dotnetCorePackages.sdk_8_0}/bin/dotnet $out/bin/dis --add-flags $out/lib/dis/dis.dll
-          wrapProgram "$out/bin/dis" \
-            --prefix PATH : ${pkgs.ffmpeg_6-full}/bin \
-            --prefix PATH : ${pkgs.yt-dlp}/bin
-        '';
-
-        runtimeDeps = [pkgs.icu];
-
-        meta = {
-          homepage = "https://github.com/DontEatOreo/dis";
-          license = pkgs.lib.licenses.agpl3Plus;
-          platforms = pkgs.lib.platforms.unix;
-          maintainers = [pkgs.lib.maintainers.donteatoreo];
-        };
-      };
-    });
-  };
+          };
+        }
+      );
+    };
 }
